f1798a693db5f66d446c1d4efe987925e07f1f6c
Umang Agarwalla <umangagarwalla111@gmail.com>
2023-04-14 17:34:57 +0530

    Create main.go

---
+++ Backend/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"Backend/Routes"
+	"context"
+	"log"
+	"os"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+var (
+	serviceName  = os.Getenv("SERVICE_NAME")
+	collectorURL = os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
+)
+
+
+func initTracer() func(context.Context) error {
+
+	secureOption := otlptracegrpc.WithInsecure()
+
+	exporter, err := otlptrace.New(
+		context.Background(),
+		otlptracegrpc.NewClient(
+			secureOption,
+			otlptracegrpc.WithEndpoint(collectorURL),
+		),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	resources, err := resource.New(
+		context.Background(),
+		resource.WithAttributes(
+			attribute.String("service.name", serviceName),
+			attribute.String("library.language", "go"),
+		),
+	)
+	if err != nil {
+		log.Printf("Could not set resources: ", err)
+	}
+
+	otel.SetTracerProvider(
+		sdktrace.NewTracerProvider(
+			sdktrace.WithSampler(sdktrace.AlwaysSample()),
+			sdktrace.WithBatcher(exporter),
+			sdktrace.WithResource(resources),
+		),
+	)
+	return exporter.Shutdown
+}
+
+func main(){
+
+	cleanup := initTracer()
+	defer cleanup(context.Background())
+	
+	r:=Routes.SetupRouter()
+	if err := r.Run(":8888"); err != nil {
+		panic(err)
+	}
+
+}
